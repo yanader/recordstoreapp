7e73b6e5c6f197f166ea716c06be2bc5f4ff04c1
Ste <mrbarrow@hotmail.co.uk>
2024-06-21 11:25:16 +0100

    POST:task6

---
+++ src/main/java/com/example/recordstoreapp/ui/addalbum/AddAlbumActivity.java
@@ -0,0 +1,14 @@
+package com.example.recordstoreapp.ui.addalbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.recordstoreapp.R;
+
+public class AddAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_album);
+    }
+}
