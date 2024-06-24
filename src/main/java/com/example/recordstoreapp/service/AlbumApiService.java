3018b58ae6cdd64733a1d5e86e5079fba9d3b003
Ste <mrbarrow@hotmail.co.uk>
2024-06-24 11:15:03 +0100

    PUT and DLETE added to AlbumApiService

--- src/main/java/com/example/recordstoreapp/service/AlbumApiService.java
+++ src/main/java/com/example/recordstoreapp/service/AlbumApiService.java
@@ -3,9 +3,7 @@ package com.example.recordstoreapp.service;
 import com.example.recordstoreapp.model.Album;
 import com.example.recordstoreapp.model.AlbumStockItem;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 import java.util.List;
 
@@ -16,4 +14,10 @@ public interface AlbumApiService {
 
     @POST("albums/add")
     Call<AlbumStockItem> addNewAlbum(@Body AlbumStockItem album);
+
+    @PUT("albums/{id}")
+    Call<AlbumStockItem> updateAlbum(@Body AlbumStockItem album, @Path("id") long id);
+
+    @DELETE("albums/{id}")
+    Call<Album> deleteAlbum(@Path("id") long id);
 }
