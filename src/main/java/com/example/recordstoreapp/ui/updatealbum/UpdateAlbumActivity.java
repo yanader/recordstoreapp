626911643c6bb7fb893efbcf2d207cdeb71f9889
Ste <mrbarrow@hotmail.co.uk>
2024-06-24 11:43:33 +0100

    update album activity added

---
+++ src/main/java/com/example/recordstoreapp/ui/updatealbum/UpdateAlbumActivity.java
@@ -0,0 +1,14 @@
+package com.example.recordstoreapp.ui.updatealbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.recordstoreapp.R;
+
+public class UpdateAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_album);
+    }
+}
